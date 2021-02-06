c7ab4195b73e64000eb2ca651ca28c8ba5ecbc8c
Val <vdecyatnik@gmail.com>
2021-02-06 08:51:07 -0800

    created server file

---
+++ Develop/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
